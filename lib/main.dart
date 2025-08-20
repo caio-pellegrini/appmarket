2b546f0b815f303de4b110a98e8f4d962cc8c13b
Caio Pellegrini <caiopellegrini97@gmail.com>
2025-08-20 01:44:53 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:appmarket/pages/lojas_page.dart';
+import 'package:appmarket/pages/produtos_page.dart';
+import 'package:appmarket/pages/saldo_page.dart';
+import 'package:appmarket/pages/historico_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ManiaX',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Roboto',
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _paginaAtual = 0;
+
+  final List<Widget> _paginas = [
+    const Placeholder(),
+    const Placeholder(),
+    const Placeholder(),
+    const Placeholder(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _paginas[_paginaAtual],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _paginaAtual,
+        onTap: (index) {
+          setState(() {
+            _paginaAtual = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.store), // ou local_convenience_store
+            label: 'Lojas',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_basket),
+            label: 'Produtos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet),
+            label:'Saldo'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'Histórico'
+          )
+        ]
+      )
+    ); 
+  }
+}
