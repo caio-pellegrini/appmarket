87898fb105fc4f6a89d49846880af6ad555d6a95
Caio Pellegrini <caiopellegrini97@gmail.com>
2025-08-22 02:05:49 -0300

    test: implement flavors

---
+++ lib/flavors/maniax_config.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class FlavorConfig {
+  static const String nomeEmpresa = 'ManiaX';
+  static const Color corPrimaria = Color.fromARGB(0, 24, 57, 188);
+  static const Color corSecundaria = Color.fromARGB(255, 184, 144, 96);
+}
