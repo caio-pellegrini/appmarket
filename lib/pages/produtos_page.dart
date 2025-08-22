8470a0cb0c4f91e7ba720556cc734f596c8d3dc8
Caio Pellegrini <caiopellegrini97@gmail.com>
2025-08-22 01:08:08 -0300

    feature: create mock of products

--- lib/pages/produtos_page.dart
+++ lib/pages/produtos_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:appmarket/models/product_model.dart';
 import 'package:appmarket/widgets/product_card.dart';
+import 'package:appmarket/mock/mock_produtos.dart';
 
 class ProdutosPage extends StatefulWidget {
   const ProdutosPage({super.key});
@@ -12,67 +13,7 @@ class ProdutosPage extends StatefulWidget {
 class _ProdutosPageState extends State<ProdutosPage> {
   bool _mostrarApenasFavoritos = false;
 
-  final List<Produto> _todosOsProdutos = [
-    Produto(
-      nome: 'Arroz Branco 5kg Tio João',
-      barcode: '7893500020165',
-      isFavorite: false,
-      imageUrl: 'http://www.eanpictures.com.br:9000/api/gtin/7893500020165',
-    ),
-    Produto(
-      nome: 'Coca-Cola Lata 350ml',
-      barcode: '7894900010015',
-      isFavorite: true,
-      imageUrl: 'http://cdn-cosmos.bluesoft.com.br/products/7894900010015',
-    ),
-    Produto(
-      nome: 'Sabonete Dove 90g',
-      barcode: '7891150064980',
-      isFavorite: false,
-      imageUrl: 'http://www.eanpictures.com.br:9000/api/gtin/7891150064980',
-    ),
-    Produto(
-      nome: 'Detergente Ypê Neutro 500ml',
-      barcode: '7896098901425',
-      isFavorite: false,
-      imageUrl: 'http://cdn-cosmos.bluesoft.com.br/products/7896098900208',
-    ),
-    Produto(
-      nome: 'Leite Italac Integral 1L',
-      barcode: '7896036097066',
-      isFavorite: true,
-      imageUrl: 'https://cdn-cosmos.bluesoft.com.br/products/7898080640611',
-    ),
-    Produto(
-      nome: 'Pão de Forma Pullman Tradicional 500g',
-      barcode: '7896004000029',
-      isFavorite: false,
-      imageUrl: 'https://cdn-cosmos.bluesoft.com.br/products/7896002302197',
-    ),
-    Produto(
-      nome: 'Banana Prata - kg',
-      barcode: '7890000000013',
-      isFavorite: false,
-    ),
-    Produto(
-      nome: 'Peito de Frango Congelado Seara 1kg',
-      barcode: '7898495380003',
-      isFavorite: false,
-      imageUrl: 'https://cdn-cosmos.bluesoft.com.br/products/7894904015108',
-    ),
-    Produto(
-      nome: 'Lasanha Bolonhesa Sadia 350g',
-      barcode: '7893000511515',
-      isFavorite: false,
-      imageUrl: 'https://cdn-cosmos.bluesoft.com.br/products/7893000674233',
-    ),
-    Produto(
-      nome: 'Ração Pedigree Cães Adultos 1kg',
-      barcode: '7896029070245',
-      isFavorite: false,
-      imageUrl: 'https://cdn-cosmos.bluesoft.com.br/products/7896029053270',
-    ),
-  ];
+  final _todosOsProdutos = mockProdutos;
 
   @override
   Widget build(BuildContext context) {
